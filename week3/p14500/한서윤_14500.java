e90cd4d6a0cd61d495246ea3f5f24966b2070617
seoyun <seoyun@localhost>
2026-02-22 03:56:15 +0900

    [한서윤] 3주차 과제 수정

--- week3/p14500/한서윤_14500.java
+++ week3/p14500/한서윤_14500.java
@@ -2,63 +2,119 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+	private static int n, m, maxVal = 0, rst = 0, v[][];
+	private static int[] dr = { -1, 1, 0, 0 }, dc = { 0, 0, -1, 1 };
+	private static boolean[][] visited;
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken()), m = Integer.parseInt(st.nextToken()), maxVal = 0;
-		int[][] v = new int[n][m];
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		v = new int[n][m];
+		visited = new boolean[n][m];
 
 		for (int i = 0; i < n; i++) { // 입력.
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < m; j++) {
 				v[i][j] = Integer.parseInt(st.nextToken());
+				maxVal = Math.max(maxVal, v[i][j]);
 			}
 		}
-		// 직사각형 형태로 범위 정하고 범위 내에서 모양 만들기.
-		for (int i = 0; i < n; i++) // 가로4 * 세로1.
-			for (int j = 0; j < m - 3; j++)
-				maxVal = Math.max(maxVal, v[i][j] + v[i][j + 1] + v[i][j + 2] + v[i][j + 3]);
-
-		for (int i = 0; i < n - 3; i++) // 가로1 * 세로4.
-			for (int j = 0; j < m; j++)
-				maxVal = Math.max(maxVal, v[i][j] + v[i + 1][j] + v[i + 2][j] + v[i + 3][j]);
-
-		for (int i = 0; i < n - 1; i++) // 정사각형. 가로2 * 세로2.
-			for (int j = 0; j < m - 1; j++)
-				maxVal = Math.max(maxVal, v[i][j] + v[i][j + 1] + v[i + 1][j] + v[i + 1][j + 1]);
-
-		for (int i = 0; i < n - 2; i++) { // 가로2 * 세로3.
-			for (int j = 0; j < m - 1; j++) {
-				// 세로 3칸 고정하고 그 왼쪽/오른쪽 칸에서 위, 중간, 아래 순으로 선택.
-				int left = v[i][j] + v[i + 1][j] + v[i + 2][j], right = v[i][j + 1] + v[i + 1][j + 1] + v[i + 2][j + 1];
-				maxVal = Math.max(maxVal, left + v[i][j + 1]);
-				maxVal = Math.max(maxVal, left + v[i + 1][j + 1]);
-				maxVal = Math.max(maxVal, left + v[i + 2][j + 1]);
-				maxVal = Math.max(maxVal, right + v[i][j]);
-				maxVal = Math.max(maxVal, right + v[i + 1][j]);
-				maxVal = Math.max(maxVal, right + v[i + 2][j]);
-				// 번개 모양.
-				maxVal = Math.max(maxVal, v[i][j] + v[i + 1][j] + v[i + 1][j + 1] + v[i + 2][j + 1]);
-				maxVal = Math.max(maxVal, v[i][j + 1] + v[i + 1][j] + v[i + 1][j + 1] + v[i + 2][j]);
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				visited[i][j] = true;
+				dfs(i, j, 1, v[i][j]);
+				visited[i][j] = false;
 			}
 		}
+		System.out.println(rst);
+	}
 
-		for (int i = 0; i < n - 1; i++) { // 가로3 * 세로2.
-			for (int j = 0; j < m - 2; j++) {
-				// 가로 3칸 고정하고 그 위/아래 칸에서 왼쪽, 가운데, 오른쪽 순으로 선택.
-				int up = v[i][j] + v[i][j + 1] + v[i][j + 2], down = v[i + 1][j] + v[i + 1][j + 1] + v[i + 1][j + 2];
-				maxVal = Math.max(maxVal, up + v[i + 1][j]);
-				maxVal = Math.max(maxVal, up + v[i + 1][j + 1]);
-				maxVal = Math.max(maxVal, up + v[i + 1][j + 2]);
-				maxVal = Math.max(maxVal, down + v[i][j]);
-				maxVal = Math.max(maxVal, down + v[i][j + 1]);
-				maxVal = Math.max(maxVal, down + v[i][j + 2]);
-				// 번개 모양.
-				maxVal = Math.max(maxVal, v[i][j] + v[i][j + 1] + v[i + 1][j + 1] + v[i + 1][j + 2]);
-				maxVal = Math.max(maxVal, v[i][j + 1] + v[i][j + 2] + v[i + 1][j + 1] + v[i + 1][j]);
+	static void dfs(int r, int c, int size, int sum) {
+		if (sum + (4 - size) * maxVal <= rst) // 이미 구한 값보다 더 커질 수 없는 경우.
+			return;
+		if (size == 4) { // 4개를 모두 방문한 경우.
+			rst = Math.max(rst, sum);
+			return;
+		}
+		for (int i = 0; i < 4; i++) {
+			int nr = r + dr[i]; // 다음 좌표 탐색. 상하좌우 순.
+			int nc = c + dc[i];
+			if (nr < 0 || nr >= n || nc < 0 || nc >= m) // 범위를 넘어가는 경우.
+				continue;
+
+			if (!visited[nr][nc]) {
+				visited[nr][nc] = true;
+				if (size == 2) // ㅏ 모양 탐색.
+					dfs(r, c, size + 1, sum + v[nr][nc]); // 다음을 방문 처리하고 제자리에서 다시 탐색.
+
+				dfs(nr, nc, size + 1, sum + v[nr][nc]);
+				visited[nr][nc] = false;
 			}
 		}
-		System.out.println(maxVal);
 	}
 }
+
+//public class Main {
+//	public static void main(String[] args) throws Exception {
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		StringTokenizer st;
+//		st = new StringTokenizer(br.readLine());
+//		int n = Integer.parseInt(st.nextToken()), m = Integer.parseInt(st.nextToken()), maxVal = 0;
+//		int[][] v = new int[n][m];
+//
+//		for (int i = 0; i < n; i++) { // 입력.
+//			st = new StringTokenizer(br.readLine());
+//			for (int j = 0; j < m; j++) {
+//				v[i][j] = Integer.parseInt(st.nextToken());
+//			}
+//		}
+//		// 직사각형 형태로 범위 정하고 범위 내에서 모양 만들기.
+//		for (int i = 0; i < n; i++) // 가로4 * 세로1.
+//			for (int j = 0; j < m - 3; j++)
+//				maxVal = Math.max(maxVal, v[i][j] + v[i][j + 1] + v[i][j + 2] + v[i][j + 3]);
+//
+//		for (int i = 0; i < n - 3; i++) // 가로1 * 세로4.
+//			for (int j = 0; j < m; j++)
+//				maxVal = Math.max(maxVal, v[i][j] + v[i + 1][j] + v[i + 2][j] + v[i + 3][j]);
+//
+//		for (int i = 0; i < n - 1; i++) // 정사각형. 가로2 * 세로2.
+//			for (int j = 0; j < m - 1; j++)
+//				maxVal = Math.max(maxVal, v[i][j] + v[i][j + 1] + v[i + 1][j] + v[i + 1][j + 1]);
+//
+//		for (int i = 0; i < n - 2; i++) { // 가로2 * 세로3.
+//			for (int j = 0; j < m - 1; j++) {
+//				// 세로 3칸 고정하고 그 왼쪽/오른쪽 칸에서 위, 중간, 아래 순으로 선택.
+//				int left = v[i][j] + v[i + 1][j] + v[i + 2][j], right = v[i][j + 1] + v[i + 1][j + 1] + v[i + 2][j + 1];
+//				maxVal = Math.max(maxVal, left + v[i][j + 1]);
+//				maxVal = Math.max(maxVal, left + v[i + 1][j + 1]);
+//				maxVal = Math.max(maxVal, left + v[i + 2][j + 1]);
+//				maxVal = Math.max(maxVal, right + v[i][j]);
+//				maxVal = Math.max(maxVal, right + v[i + 1][j]);
+//				maxVal = Math.max(maxVal, right + v[i + 2][j]);
+//				// 번개 모양.
+//				maxVal = Math.max(maxVal, v[i][j] + v[i + 1][j] + v[i + 1][j + 1] + v[i + 2][j + 1]);
+//				maxVal = Math.max(maxVal, v[i][j + 1] + v[i + 1][j] + v[i + 1][j + 1] + v[i + 2][j]);
+//			}
+//		}
+//
+//		for (int i = 0; i < n - 1; i++) { // 가로3 * 세로2.
+//			for (int j = 0; j < m - 2; j++) {
+//				// 가로 3칸 고정하고 그 위/아래 칸에서 왼쪽, 가운데, 오른쪽 순으로 선택.
+//				int up = v[i][j] + v[i][j + 1] + v[i][j + 2], down = v[i + 1][j] + v[i + 1][j + 1] + v[i + 1][j + 2];
+//				maxVal = Math.max(maxVal, up + v[i + 1][j]);
+//				maxVal = Math.max(maxVal, up + v[i + 1][j + 1]);
+//				maxVal = Math.max(maxVal, up + v[i + 1][j + 2]);
+//				maxVal = Math.max(maxVal, down + v[i][j]);
+//				maxVal = Math.max(maxVal, down + v[i][j + 1]);
+//				maxVal = Math.max(maxVal, down + v[i][j + 2]);
+//				// 번개 모양.
+//				maxVal = Math.max(maxVal, v[i][j] + v[i][j + 1] + v[i + 1][j + 1] + v[i + 1][j + 2]);
+//				maxVal = Math.max(maxVal, v[i][j + 1] + v[i][j + 2] + v[i + 1][j + 1] + v[i + 1][j]);
+//			}
+//		}
+//		System.out.println(maxVal);
+//	}
+//}
--- week3/p15686/한서윤_15686.java
+++ week3/p15686/한서윤_15686.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	private static int m, pick[], homeCnt, chickenCnt, minVal;
+	private static int m, pick[], minVal;
 	private static ArrayList<int[]> home, chicken;
 
 	public static void main(String[] args) throws Exception {
@@ -11,21 +11,18 @@ public class Main {
 		st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
-		String[] map = new String[n];
 		home = new ArrayList<>();
 		chicken = new ArrayList<>();
 		for (int i = 0; i < n; i++) { // 입력.
-			map[i] = br.readLine();
+			String str = br.readLine(); // 2차원 배열 삭제.
 			for (int j = 0; j < n * 2; j += 2) {
-				if (map[i].charAt(j) == '1')
+				if (str.charAt(j) == '1')
 					home.add(new int[] { i, j / 2 });
-				else if (map[i].charAt(j) == '2')
+				else if (str.charAt(j) == '2')
 					chicken.add(new int[] { i, j / 2 });
 			}
 		}
 		pick = new int[m];
-		homeCnt = home.size();
-		chickenCnt = chicken.size();
 		minVal = Integer.MAX_VALUE;
 		combi(0, 0);
 		System.out.println(minVal);
@@ -34,10 +31,10 @@ public class Main {
 	static void combi(int start, int cnt) { // 치킨집 고르기.
 		if (cnt == m) {
 			int sum = 0;
-			for (int i = 0; i < homeCnt; i++) {
-				int cal = Integer.MAX_VALUE, r = home.get(i)[0], c = home.get(i)[1];
-				for (int j = 0; j < m; j++) {
-					int dis = Math.abs(r - chicken.get(pick[j])[0]) + Math.abs(c - chicken.get(pick[j])[1]);
+			for (int[] i : home) { // for-each 구문으로 수정.
+				int cal = Integer.MAX_VALUE, r = i[0], c = i[1];
+				for (int j : pick) { // for-each 구문으로 수정.
+					int dis = Math.abs(r - chicken.get(j)[0]) + Math.abs(c - chicken.get(j)[1]);
 					cal = Math.min(cal, dis);
 				}
 				sum += cal;
@@ -45,7 +42,7 @@ public class Main {
 			minVal = Math.min(minVal, sum);
 			return;
 		}
-		for (int i = start; i < chickenCnt; i++) {
+		for (int i = start; i < chicken.size(); i++) {
 			pick[cnt] = i;
 			combi(i + 1, cnt + 1);
 		}
