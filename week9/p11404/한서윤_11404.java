abd1957dc6c9b29bedda1fc0b172dd95fa00d3af
yyrhsc@naver.com <seoyun@localhost>
2026-04-20 02:56:29 +0900

    [한서윤] 9주차 11404번 제출

--- week9/p11404/한서윤_11404.java
+++ week9/p11404/한서윤_11404.java
@@ -1,85 +1,47 @@
 import java.io.*;
 import java.util.*;
 
-class Pos { // 위치 저장.
-	int x, y;
-
-	Pos(int x, int y) {
-		this.x = x;
-		this.y = y;
-	}
-}
-
-class Turn { // 방향 전환.
-	int time;
-	String dir;
-
-	Turn(int time, String dir) {
-		this.time = time;
-		this.dir = dir;
-	}
-}
-
 public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		int n = Integer.parseInt(br.readLine()), k = Integer.parseInt(br.readLine());
-		int board[][] = new int[n + 1][n + 1]; // 0: 빈 칸, 1: 뱀, 2: 사과.
-		for (int i = 0; i < k; i++) {
+		int n = Integer.parseInt(br.readLine()), m = Integer.parseInt(br.readLine());
+		long cost[][] = new long[n + 1][n + 1];
+
+		// 플로이드-워셜 수행을 위해 최댓값의 절반값으로 배열 초기화.
+		for (int i = 1; i <= n; i++)
+			Arrays.fill(cost[i], Long.MAX_VALUE / 2);
+
+		// 자기 자신에게 가는 비용.
+		for (int i = 1; i <= n; i++)
+			cost[i][i] = 0;
+
+		// 입력.
+		for (int i = 0; i < m; i++) {
 			st = new StringTokenizer(br.readLine());
-			int r = Integer.parseInt(st.nextToken()), c = Integer.parseInt(st.nextToken());
-			board[r][c] = 2;
+			int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken()),
+					c = Integer.parseInt(st.nextToken());
+			cost[a][b] = Math.min(cost[a][b], c);
 		}
-		int l = Integer.parseInt(br.readLine());
-		Queue<Turn> q = new ArrayDeque<>(); // 방향 전환 저장.
-		for (int i = 0; i < l; i++) {
-			st = new StringTokenizer(br.readLine());
-			int x = Integer.parseInt(st.nextToken());
-			String c = st.nextToken();
-			q.add(new Turn(x, c));
-		}
-		Deque<Pos> snake = new ArrayDeque<>();
-		snake.add(new Pos(1, 1));
-		board[1][1] = 1;
 
-		int dr[] = { -1, 0, 1, 0 }, dc[] = { 0, 1, 0, -1 }; // 상 우 하 좌.
-		int time = 1, dir = 1; // 처음 진행 방향 오른쪽.
-		while (true) { // 시뮬레이션.
-			int nr = snake.peek().x + dr[dir], nc = snake.peek().y + dc[dir];
-			if (nr <= 0 || nr > n || nc <= 0 || nc > n) // 벽에 부딪히면 게임 종료.
-				break;
-			if (board[nr][nc] == 1) // 자기자신의 몸과 부딪히면 게임 종료.
-				break;
-
-			if (board[nr][nc] == 0) { // 꼬리 한 칸 앞으로 전진.
-				Pos tail = snake.pollLast();
-				board[tail.x][tail.y] = 0;
-			}
-			// else if (board[nr][nc] == 2) ; // 사과를 먹으면 한 칸 늘어남. (꼬리 움직일 필요 x.)
-			snake.addFirst(new Pos(nr, nc)); // 머리 앞으로 이동.
-			board[nr][nc] = 1;
-
-			if (!q.isEmpty() && time == q.peek().time) { // 방향 전환 확인.
-				switch (q.poll().dir) {
-				case "L": // 왼쪽으로 회전.
-					dir = (dir + 3) % 4;
-					break;
-				case "D": // 오른쪽으로 회전.
-					dir = (dir + 1) % 4;
-					break;
+		// 플로이드-워셜 수행.
+		for (int k = 1; k <= n; k++) {
+			for (int i = 1; i <= n; i++) {
+				for (int j = 1; j <= n; j++) {
+					if (cost[i][k] != Long.MAX_VALUE / 2 && cost[k][j] != Long.MAX_VALUE / 2)
+						cost[i][j] = Math.min(cost[i][j], cost[i][k] + cost[k][j]);
 				}
 			}
-			time++;
 		}
-		System.out.println(time);
-	}
 
-	static void printMap(int time, int n, int board[][]) { // 확인용.
-		System.out.println(time);
-		for (int i = 0; i < n; i++) {
-			System.out.println(Arrays.toString(board[i]));
+		// 출력.
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				if (cost[i][j] == Long.MAX_VALUE / 2)
+					cost[i][j] = 0;
+				System.out.print(cost[i][j] + " ");
+			}
+			System.out.println();
 		}
-		System.out.println();
 	}
 }
--- week9/p17070/한서윤_17070.java
+++ week9/p17070/한서윤_17070.java
@@ -1,71 +1,10 @@
 import java.io.*;
 import java.util.*;
 
-class Rupee implements Comparable<Rupee> {
-	int r, c, size;
-
-	Rupee(int r, int c, int size) {
-		this.r = r;
-		this.c = c;
-		this.size = size;
-	}
-
-	@Override
-	public int compareTo(Rupee o) {
-		return Integer.compare(this.size, o.size);
-	}
-}
-
 public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		int dr[] = { -1, 1, 0, 0 }, dc[] = { 0, 0, -1, 1 };
-		StringBuilder rst = new StringBuilder();
-
-		int t = 1, n, board[][], cost[][], r, c, size;
-		boolean visited[][];
-		PriorityQueue<Rupee> pq = new PriorityQueue<>(); // 선택 가능한 위치 후보. 비용이 적은 순으로 정렬.
-		while (true) {
-			n = Integer.parseInt(br.readLine());
-			if (n == 0)
-				break;
-			board = new int[n][n];
-			cost = new int[n][n];
-			visited = new boolean[n][n];
-			for (int i = 0; i < n; i++) // 최댓값으로 초기화.
-				Arrays.fill(cost[i], Integer.MAX_VALUE);
-
-			for (int i = 0; i < n; i++) { // 동굴 현황 입력.
-				st = new StringTokenizer(br.readLine());
-				for (int j = 0; j < n; j++)
-					board[i][j] = Integer.parseInt(st.nextToken());
-			}
-
-			pq.add(new Rupee(0, 0, board[0][0])); // 시작점 넣기.
-
-			while (!pq.isEmpty()) {
-				r = pq.peek().r;
-				c = pq.peek().c;
-				size = pq.poll().size;
-				if (visited[r][c])
-					continue; // 이미 선택한 위치.
-
-				visited[r][c] = true; // 방문 처리.
-				cost[r][c] = size; // 최소 비용 업데이트.
-
-				for (int i = 0; i < 4; i++) { // 상하좌우 탐색. 아직 선택하지 않은 위치 추가.
-					int nr = r + dr[i], nc = c + dc[i];
-					if (nr < 0 || nr >= n || nc < 0 || nc >= n)
-						continue;
-					if (visited[nr][nc]) // 비용 계산이 끝난 위치는 다시 계산할 필요 없음. (가장 최소인 값만 갱신했기 때문에 더 작아질 수 x.)
-						continue;
-					pq.add(new Rupee(nr, nc, size + board[nr][nc]));
-				}
-			}
-			rst.append("Problem ").append(t).append(": ").append(cost[n - 1][n - 1]).append("\n");
-			t++;
-		}
-		System.out.println(rst);
+		
 	}
 }
