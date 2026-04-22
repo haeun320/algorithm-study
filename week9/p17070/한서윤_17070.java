362382c1073a06962fc6e412825239ddb5943e13
coolpiece <90741842+coolpiece@users.noreply.github.com>
2026-04-22 10:53:01 +0900

    [한서윤] 17070번 수정
    [한서윤] 17070번 수정

--- week9/p17070/한서윤_17070.java
+++ week9/p17070/한서윤_17070.java
@@ -33,12 +33,30 @@ public class Main {
 					dp[2][r][c] += dp[0][r - 1][c - 1] + dp[1][r - 1][c - 1] + dp[2][r - 1][c - 1];
 			}
 		}
-		
-//		for (int i = 0; i < 3; i++) {
-//			for (int j = 0; j <= n; j++)
-//				System.out.println(Arrays.toString(dp[i][j]));
-//			System.out.println();
-//		}
+
+		// 아래 방식으로도 가능.
+		// for (int r = 1; r <= n; r++) {
+		// 	for (int c = 3; c <= n; c++) {
+		// 		// 벽이면 패스.
+		// 		if (home[r][c] == 1)
+		// 			continue;
+
+		// 		// 가로로 도착하는 경우.
+		// 		dp[0][r][c] = dp[0][r][c - 1] + dp[2][r][c - 1];
+		// 		// 세로로 도착하는 경우.
+		// 		dp[1][r][c] = dp[1][r - 1][c] + dp[2][r - 1][c];
+		// 		// 대각선으로 도착하는 경우.
+		// 		if (home[r - 1][c] != 1 && home[r][c - 1] != 1)
+		// 			dp[2][r][c] = dp[0][r - 1][c - 1] + dp[1][r - 1][c - 1] + dp[2][r - 1][c - 1];
+		// 	}
+		// }
+
+		// 확인용 출력.
+		// for (int i = 0; i < 3; i++) {
+		// 	for (int j = 0; j <= n; j++)
+		// 		System.out.println(Arrays.toString(dp[i][j]));
+		// 	System.out.println();
+		// }
 		
 		int rst = 0;
 		for (int i = 0; i < 3; i++) // 가로, 세로, 대각선으로 도착한 경우를 합치면 됨.
