b602e793224fe0a16abd7c7129a9a65a6f81883e
yyrhsc@naver.com <seoyun@localhost>
2026-04-25 21:37:55 +0900

    [10주차] 한서윤 과제 제출

---
+++ week10/p17144/한서윤_17144.java
@@ -0,0 +1,132 @@
+import java.io.*;
+import java.util.*;
+
+class Dust {
+	int r, c, amount;
+
+	Dust(int r, int c, int amount) {
+		this.r = r;
+		this.c = c;
+		this.amount = amount;
+	}
+}
+
+public class Main {
+	static int dr[] = { -1, 0, 1, 0 }, dc[] = { 0, 1, 0, -1 }; // 상, 우, 하, 좌
+	static int r, c, room[][], up, down;
+	static Queue<Dust> q;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		r = Integer.parseInt(st.nextToken());
+		c = Integer.parseInt(st.nextToken());
+		int t = Integer.parseInt(st.nextToken());
+		room = new int[r][c];
+		q = new ArrayDeque<>(); // 미세먼지 위치 저장.
+
+		up = -1;
+		down = -1;
+		for (int i = 0; i < r; i++) { // 입력.
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < c; j++) {
+				room[i][j] = Integer.parseInt(st.nextToken());
+				if (room[i][j] > 0)
+					q.add(new Dust(i, j, room[i][j]));
+				if (room[i][j] == -1) { // 공기청정기 위치 저장.
+					if (up == -1)
+						up = i;
+					else
+						down = i;
+				}
+			}
+		}
+
+		int rst = 0;
+		for (int i = 1; i <= t; i++) {
+			solve();
+			if (i == t) { // t초가 모두 지난 경우.
+				for (int j = 0; j < r; j++) {
+					for (int k = 0; k < c; k++) {
+						if (room[j][k] > 0)
+							rst += room[j][k];
+					}
+				}
+				break;
+			}
+
+			// 다음 수행을 위해 다시 큐에 넣기.
+			for (int j = 0; j < r; j++) {
+				for (int k = 0; k < c; k++) {
+					if (room[j][k] > 0)
+						q.add(new Dust(j, k, room[j][k]));
+				}
+			}
+		}
+		System.out.println(rst);
+	}
+
+	static void solve() {
+		while (!q.isEmpty()) { // 미세먼지 확산.
+			Dust cur = q.poll();
+			int cnt = 0;
+			for (int i = 0; i < 4; i++) {
+				int nr = cur.r + dr[i], nc = cur.c + dc[i];
+				if (nr < 0 || nr >= r || nc < 0 || nc >= c) // 범위를 넘어가는 경우.
+					continue;
+				if (room[nr][nc] == -1) // 공기청정기가 있는 경우.
+					continue;
+				room[nr][nc] += cur.amount / 5;
+				cnt++;
+			}
+			room[cur.r][cur.c] -= cur.amount / 5 * cnt;
+		}
+
+		// 위쪽 공기청정기 바람.
+		int nr = up - 1, nc = 0;
+		for (int i = 0; i < 4; i++) { // 상, 우, 하, 좌 순으로 이동.
+			while (true) { // 벽에 닿으면 방향 전환.
+				if (nr + dr[i] < 0 || nr + dr[i] >= down || nc + dc[i] < 0 || nc + dc[i] >= c)
+					break;
+				if (room[nr + dr[i]][nc + dc[i]] == -1) {
+					room[nr][nc] = 0;
+					break;
+				}
+				room[nr][nc] = room[nr + dr[i]][nc + dc[i]];
+				nr += dr[i];
+				nc += dc[i];
+			}
+		}
+
+		// 아래쪽 공기청정기 바람.
+		nr = down + 1;
+		nc = 0;
+		for (int i = 2; i >= -1; i--) { // 하, 우, 상, 좌 순으로 이동.
+			if (i == -1)
+				i = 3;
+			while (true) { // 벽에 닿으면 방향 전환.
+				if (nr + dr[i] < down || nr + dr[i] >= r || nc + dc[i] < 0 || nc + dc[i] >= c)
+					break;
+				if (room[nr + dr[i]][nc + dc[i]] == -1) {
+					room[nr][nc] = 0;
+					break;
+				}
+				room[nr][nc] = room[nr + dr[i]][nc + dc[i]];
+				nr += dr[i];
+				nc += dc[i];
+			}
+			if (i == 3)
+				break;
+		}
+	}
+
+	static void printRoom() { // 확인용.
+		for (int i = 0; i < r; i++) {
+			for (int j = 0; j < c; j++)
+				System.out.print(room[i][j] + " ");
+			System.out.println();
+		}
+		System.out.println();
+	}
+}
---
+++ week10/p17616/한서윤_17616.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static boolean visited[];
+	static int u, v, cnt;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken()), m = Integer.parseInt(st.nextToken()),
+				x = Integer.parseInt(st.nextToken());
+		ArrayList<Integer> better[] = new ArrayList[n + 1]; // better[i]: i보다 등수가 더 높은 학생들.
+		ArrayList<Integer> worse[] = new ArrayList[n + 1]; // worse[i]: i보다 등수가 더 낮은 학생들.
+
+		for (int i = 1; i <= n; i++) {
+			better[i] = new ArrayList<>();
+			worse[i] = new ArrayList<>();
+		}
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken());
+			worse[a].add(b); // a보다 b의 등수가 더 낮음.
+			better[b].add(a); // b보다 a의 등수가 더 높음.
+		}
+
+		u = 1; // 가능한 가장 높은 등수.
+		v = n; // 가능한 가장 낮은 등수.
+		cnt = 0; // x보다 등수가 높은/낮은 학생의 수.
+
+		visited = new boolean[n + 1];
+		dfs(better, x);
+		u += cnt; // 최대 등수 내리기.
+
+		Arrays.fill(visited, false);
+		cnt = 0;
+		dfs(worse, x);
+		v -= cnt; // 최소 등수 올리기.
+		System.out.println(u + " " + v);
+	}
+
+	static void dfs(ArrayList<Integer>[] rank, int cur) {
+		for (int next : rank[cur]) {
+			if (!visited[next]) {
+				visited[next] = true;
+				cnt++;
+				dfs(rank, next);
+			}
+		}
+	}
+}
